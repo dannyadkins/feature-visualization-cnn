4316dc4a3951266df05598bef03c062b7963d0a5
danielpadkins <danielpadk@gmail.com>
2023-11-19 01:03:33 -0500

    not working yet

---
+++ visualization.py
@@ -0,0 +1,68 @@
+import torch 
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from cnn import CNN 
+from torch.utils.data import dataloader
+from torchvision import datasets, transforms
+
+def visualize_for_neuron(model: CNN, layer_name: str, neuron_idx: int, train_loader, save: bool =False, num_iterations: int = 1000, lr = 0.01):
+    # Load the pre-trained model
+    model.eval()
+
+    # get one image from train loader
+    img, label = next(iter(train_loader))
+    img_pil = transforms.ToPILImage()(img.squeeze())
+    img.requires_grad = True
+
+    # Define optimizer
+    optimizer = torch.optim.Adam([img], lr=lr)
+
+    for i in range(num_iterations):
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass of the model 
+        output = model(img)
+
+        # Get the activation of the specified layer for the specified neuron
+        activation = output[0, neuron_idx]
+
+        # Compute the loss as the negative activation
+        loss = -activation
+
+        # Backward pass
+        loss.backward()
+
+        print("Iteration: {}, Loss: {}".format(i, loss.item()))
+
+        # Update the image
+        optimizer.step()
+
+    # Convert the optimized image to a numpy array
+    img_np = img.detach().squeeze().numpy()
+
+    # Visualize the optimized image
+    plt.imshow(img_pil)
+    plt.axis('off')
+    plt.show()
+
+    # Save the optimized image if desired
+    if save:
+        img_pil.save('optimized_image.jpg')
+
+if __name__ == '__main__':
+    # Load the pre-trained model
+    model = CNN()
+    model.load_state_dict(torch.load('mnist_cnn.pt'))
+
+    train_loader = dataloader.DataLoader(
+        datasets.MNIST('./data', train=True, download=True,
+                        transform=transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.1307,), (0.3081,)) # mean, std
+                        ])),
+        batch_size=1, shuffle=True)
+
+
+    # Visualize the first neuron in the first convolutional layer
+    visualize_for_neuron(model, 'conv1', 1, train_loader, save=True)
