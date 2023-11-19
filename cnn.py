4f27247a5fb6f69206a29a296d9f8d5b67cd4184
danielpadkins <danielpadk@gmail.com>
2023-11-19 00:30:35 -0500

    model scaffolding try 1

---
+++ cnn.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = nn.functional.relu(nn.functional.max_pool2d(self.conv1(x), 2))
+        x = nn.functional.relu(nn.functional.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 320)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return nn.functional.log_softmax(x, dim=1)
--- train_cnn.py
+++ train_cnn.py
@@ -2,6 +2,29 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import dataloader
 
 from cnn import CNN
+
+# small toy model 
+hyperparams = {
+    'batch_size': 64,
+    'test_batch_size': 1000,
+    'epochs': 10,
+    'lr': 0.01,
+    'momentum': 0.5,
+    'no_cuda': True,
+    'seed': 1,
+    'log_interval': 100,
+    'save_model': True
+}
+
+# load data
+train_loader = dataloader.DataLoader(
+    datasets.MNIST('./data', train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,)) # mean, std
+                    ])),
+    batch_size=hyperparams['batch_size'], shuffle=True)
+
